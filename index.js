20840d8f0ef14789ec29163c41ada4c3fc33f970
Mananas <Mananas>
2024-11-11 12:12:00 +0100

    Update

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 console.log("Greeting loaded");
 
 function greeting() {
-  const question = "Dime tu nombre:";
-  const userName = prompt(question);
-  if (userName === null || userName === "") return;
+    const question = "Dime tu nombre:";
+    const userName = prompt(question);
+    if (userName === null || userName === "") return;
 
-  const message = `Buenos días ${userName}`;
-  alert(message);
+    const message = `Buenos días ${userName}`;
+    alert(message);
 }
 
 const button = document.querySelector("button");
 button.addEventListener("click", greeting);
+
+/**
+ * Errores comunes
+ *
+ * Reference error: Llamada a elemento no definido.
+ * Type error: Error de utilización en un tipo.
+ *
+ */
