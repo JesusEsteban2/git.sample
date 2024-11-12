28d4a3798b0b214d95c41e44223f7676a9f96b48
Mananas <Mananas>
2024-11-12 12:07:50 +0100

    Update

--- index.js
+++ index.js
@@ -19,3 +19,34 @@ button.addEventListener("click", greeting);
  * Type error: Error de utilización en un tipo.
  *
  */
+
+function ifEncadenados() {
+    const age = 12,
+        userName = "Pepe",
+        gender = "";
+
+    if (age < 10) {
+        console.log("Los niños no pueden entrar");
+    } else if (age < 18) {
+        console.log("Los menores deben estar acopañados");
+    } else {
+        console.log("Buenos días ", userName);
+    }
+}
+
+function switchCase(numero) {
+    switch (numero) {
+        case 1:
+            return "Uno";
+            break;
+        case 2:
+            return "Dos";
+            break;
+        case 3:
+            return "Tres";
+            break;
+        default:
+            return "Otro número";
+    }
+}
+// switch .. case .. default:
